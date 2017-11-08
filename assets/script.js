8a2ccff42fa116dc35dcbcb725146c58b0261e55
Kong Le <kongle989@yahoo.com>
2017-11-07 21:12:38 -0500

    drew colored rectangle, line, and circle

---
+++ assets/script.js
@@ -0,0 +1,14 @@
+$(window).on("load", function () {
+
+    let canvas = $("#myCanvas")[0],
+        ctx = canvas.getContext("2d");
+    ctx.fillStyle = "#FF0000";
+    ctx.fillRect(0, 0, 150, 75);
+    ctx.moveTo(0,0);
+    ctx.lineTo(200,100);
+    ctx.stroke();
+    ctx.beginPath();
+    ctx.arc(95,50,40,0,2*Math.PI);
+    ctx.stroke();
+
+});
